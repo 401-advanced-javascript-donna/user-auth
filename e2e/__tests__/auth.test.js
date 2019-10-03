dca47a87af9b1aa49e8b91e765b2dbb57926af91
sanvean74 <dlambertpdx@gmail.com>
2019-10-03 15:09:53 -0700

    signin route test passing

--- e2e/__tests__/auth.test.js
+++ e2e/__tests__/auth.test.js
@@ -33,5 +33,33 @@ describe('Auth API', () => {
         expect(body.error).toBe('Email me@me.com already in use');
       });
   });
+
+  function testEmailAndPasswordRequired(route, testProperty, user) {
+    it(`${route} requires ${testProperty}`, () => {
+      return request
+        .post(`/api/auth/${route}`)
+        .send(user)
+        .expect(400)
+        .then(({ body }) => {
+          expect(body.error).toBe('Email and password required');
+        });
+    });
+  }
+
+  testEmailAndPasswordRequired('signup', 'email', { password: 'I no like emails' });
+  testEmailAndPasswordRequired('signup', 'password', { email: 'no@password.com ' });
+  testEmailAndPasswordRequired('signin', 'email', { password: 'I no like emails' });
+  testEmailAndPasswordRequired('signup', 'password', { email: 'no@password.com' });
+
+  it('signs in a user', () => {
+    return request
+      .post('/api/auth/signin')
+      .send(testUser)
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.token).toBeDefined();
+      });
+  });
+
   
 });

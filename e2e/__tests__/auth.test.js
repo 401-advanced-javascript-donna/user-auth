775cfbdb6880c0656836cbc1df89f985f40e992e
sanvean74 <dlambertpdx@gmail.com>
2019-10-03 14:26:53 -0700

    get verify and post signup passing

---
+++ e2e/__tests__/auth.test.js
@@ -0,0 +1,26 @@
+const request = require('../request');
+const { dropCollection } = require('../db');
+const jwt = require('jsonwebtoken');
+const { signupUser } = require('../data-helpers');
+
+describe('Auth API', () => {
+
+  beforeEach(() => dropCollection('users'));
+
+  const testUser = {
+    email: 'me@me.com',
+    password: 'abc'
+  };
+
+  let user = null;
+
+  beforeEach(() => {
+    return signupUser(testUser)
+      .then(newUser => user = newUser);
+  });
+
+  it('signs up a user', () => {
+    expect(user.token).toBeDefined();
+  });
+  
+});

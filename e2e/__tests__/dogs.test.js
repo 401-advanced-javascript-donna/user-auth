8fe60b194c2afe9b83cf466bf6300b848d4a8f89
sanvean74 <dlambertpdx@gmail.com>
2019-10-07 16:27:17 -0700

    working on getting update to work

--- e2e/__tests__/dogs.test.js
+++ e2e/__tests__/dogs.test.js
@@ -1,6 +1,5 @@
 const request = require('../request');
 const { dropCollection } = require('../db');
-const { signupUser } = require('../data-helpers');
 const User = require('../../lib/models/user');
 
 describe('Dogs API', () => {
@@ -16,12 +15,12 @@ describe('Dogs API', () => {
     name: 'Ren Hoek'
   };
 
-  function postDog(doggo) {
+  const postDoggo = doggo => {
     return request
       .post('/api/dogs')
       .set('Authorization', user.token)
       .send(doggo);
-  }
+  };
 
   let user = null;
 
@@ -36,7 +35,7 @@ describe('Dogs API', () => {
             roles: 'admin'
           }
         })
-          .then(updateUser => {
+          .then(() => {
             return request
               .post('/api/auth/signin')
               .send(testUser)
@@ -49,7 +48,7 @@ describe('Dogs API', () => {
   });
 
   it('admin can post dogs', () => {
-    return postDog(doggo).then(({ body }) => {
+    return postDoggo(doggo).then(({ body }) => {
       expect(body).toMatchInlineSnapshot(
         {
           _id: expect.any(String)
@@ -65,16 +64,55 @@ describe('Dogs API', () => {
     });
   });
 
-  it.skip('admin can update a doggo', () => {
-    console.log(doggo.name)
-    return postDog(doggo).then(({ body }) => {
+  it.skip('admin can update dogs', () => {
+    return postDoggo(doggo)
+      .then(doggo => {
+        doggo.name = 'Ren Hoooeeek';
+        return request
+          .put(`/api/dogs/${doggo._id}`)
+          .set('Authorization', user.token)
+          .send(doggo)
+          .expect(200);
+      })
+      .then(({ body }) => {
+        expect(body.name).toBe('Ren Hoooeeek');
+      });
+  });
+
+  it('admin can delete dogs', () => {
+    return postDoggo(doggo).then(({ body }) => {
       return request
-        .put(`/api/dogs/${body._id}`)
+        .delete(`/api/dogs/${body._id}`)
         .set('Authorization', user.token)
-        .send({ name: 'Ren Höek' })
+        .expect(200);
+    });
+  });
+
+  it('any user can get all dogs', () => {
+    return Promise.all([
+      postDoggo(doggo),
+      postDoggo(doggo),
+      postDoggo(doggo)
+    ]).then(() => {
+      return request
+        .get('/api/dogs')
+        .set('Authorization', user.token)
         .expect(200)
         .then(({ body }) => {
-          expect(body.name).toBe('Ren Höek');
+          expect(body.length).toBe(3);
+          expect(body[0]).toMatchInlineSnapshot(
+            {
+              _id: expect.any(String)
+            },
+
+            `
+            Object {
+              "__v": 0,
+              "_id": Any<String>,
+              "name": "Ren Hoek",
+            }
+          `
+          );
         });
     });
   });
--- scripts/make-admin.js
+++ scripts/make-admin.js
@@ -22,3 +22,32 @@ User.updateById(
   .then(console.log)
   .catch(console.log)
   .finally(() => mongoose.connection.close());
+
+// require('dotenv').config();
+// const connect = require('../lib/connect');
+// require('../lib/models/register-plugins');
+// const User = require('../lib/models/user');
+// const mongoose = require('mongoose');
+// const bcrypt = require('bcrypt');
+
+// connect(process.env.MONGODB_URI);
+
+// User.create({
+//   email: 'admin@admin.com',
+//   hash: bcrypt.hashSync('password', 8),
+//   roles: []
+// }).then(user => {
+//   User.updateById(
+//     user._id,
+//     { 
+//       $addToSet: { 
+//         roles: 'admin'
+//       }
+//     }
+//   )
+//     .then(result => {
+//       console.log(result);
+//     })
+//     .catch()
+//     .finally(() => mongoose.connection.close());
+// });
